fb8c0344da885455825dc2d47f6bf7955083a04d
andreyDelay <frowzygleb@gmail.com>
2022-05-31 23:36:53 +0300

    response body removed

--- src/main/java/com/andrey/gifcurrencyservice/controller/CurrencyController.java
+++ src/main/java/com/andrey/gifcurrencyservice/controller/CurrencyController.java
@@ -2,18 +2,15 @@ package com.andrey.gifcurrencyservice.controller;
 
 import com.andrey.gifcurrencyservice.service.CurrencyService;
 import lombok.AllArgsConstructor;
-import okhttp3.ResponseBody;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import java.io.IOException;
-import java.io.InputStream;
 
 @RestController
 @RequestMapping(value = "/api/v1/currency/rates")
@@ -25,12 +22,10 @@ public class CurrencyController {
 
 	@GetMapping(value = "/changes", produces = MediaType.IMAGE_GIF_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<?> getGif(@Valid @Pattern(regexp = "[A-Z]{3}")
+	public byte [] getGif(@Valid @Pattern(regexp = "[A-Z]{3}")
 										@RequestParam("charCode") String currencyCode) throws IOException {
-		ResponseBody gifBytesOnCurrencyRateCondition = currencyService.getGifOnCurrencyRateCondition(currencyCode);
-		InputStream inputStream = gifBytesOnCurrencyRateCondition.byteStream();
-		byte[] bytes = IOUtils.toByteArray(inputStream);
-		return new ResponseEntity<>(bytes, HttpStatus.OK);
+		return IOUtils.toByteArray(
+				currencyService.getGifOnCurrencyRateCondition(currencyCode)
+				.byteStream());
 	}
-
 }
